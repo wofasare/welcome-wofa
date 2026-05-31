7dcefa71a0053ff75c429cefe48bf99b07e1483d
wofasare <wofasare13@gmail.com>
2026-05-31 12:26:05 +0000

    Initial commit - welcome React app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1>Welcome, Wofa Asare! 🎉</h1>
+      <p style={{ fontSize: '1.2rem', color: '#555' }}>
+        You're now a developer. Let's build something amazing.
+      </p>
+    </div>
+  )
+}
+
+export default App
